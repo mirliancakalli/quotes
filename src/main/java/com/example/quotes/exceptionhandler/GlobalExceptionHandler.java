62220a5b4bf256a2eb0934faabcdc0446e683126
mirliancakalli <mirliancakalli@gmail.com>
2024-03-13 10:44:24 +0100

    logging error on global ex

--- src/main/java/com/example/quotes/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/quotes/exceptionhandler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.quotes.exceptionhandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,13 +10,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler{
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
     @ExceptionHandler(QuotationException.class)
     public ResponseEntity<String> handleQuotationException(QuotationException ex) {
+        logger.error("An unexpected error occurred: {}", ex.getMessage(), ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
+        logger.error("Quotation exception occurred: {}", ex.getMessage(), ex);
         return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
